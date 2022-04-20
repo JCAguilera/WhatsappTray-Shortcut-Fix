441d29b2f95df4bed98ea4c46518dafcc378626a
JuanCarlos Aguilera <jcaaguilerat@gmail.com>
2022-04-20 11:42:12 -0400

    Create shorcut if multiple whatsapp desktop folders exists

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ import { create } from "windows-shortcuts";
 import { readdirSync } from "fs";
 
 const dir = "C:/Program Files/WindowsApps";
-const files = readdirSync(dir);
-const wsFolder = files.find((name) => name.includes("WhatsApp"));
-const wsDir = `${dir}/${wsFolder}/app/WhatsApp.exe`;
+const files = readdirSync(dir); // List all windows apps
+const wsFolders = files.filter((name) => name.includes("WhatsAppDesktop")); // Look for WhatsApp Desktop
 
+// Get files and folders count for each WhatsApp Desktop folder
+const wsFolderMap = {};
+for (const wsFolder of wsFolders) {
+  const contents = readdirSync(`${dir}/${wsFolder}/app`);
+  wsFolderMap[wsFolder] = contents.length;
+}
+
+// Get folder with most items in it
+const wsFolder = Object.keys(wsFolderMap).reduce((a, b) => wsFolderMap[a] > wsFolderMap[b] ? a : b);
+
+// Create the shorcut
+const wsDir = `${dir}/${wsFolder}/app/WhatsApp.exe`;
 create(
   "%APPDATA%/Microsoft/Windows/Start Menu/Programs/WhatsApp/WhatsApp.lnk",
   wsDir,
